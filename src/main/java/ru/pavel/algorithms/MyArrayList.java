7389ad318f1399df17d188daed7484eea2a4e3ac
Pavel <pavelmth@gmail.com>
2019-02-28 23:43:39 +0300

    L02_fin

--- src/main/java/ru/pavel/algorithms/MyArrayList.java
+++ src/main/java/ru/pavel/algorithms/MyArrayList.java
@@ -5,50 +5,87 @@ public class MyArrayList<T> {
     private Object[] array2;
     private int last = 0;
 
-    public void MyArrayList (T object) {
-
-    }
-
-    public void add(T item) {
+    public boolean add (T item) {
         if(outOfBounds(last)) {
             array[last] = item;
+            if (array.length == last + 1) {
+                array2 = new Object[last*2];
+                System.arraycopy(array, 0 , array2, 0, array.length);
+                array = new Object[last*2];
+                System.arraycopy(array2, 0 , array, 0, last);
+                array2 = null;
+            }
             last++;
+            return true;
         } else {
             array2 = new Object[last*2];
             System.arraycopy(array, 0 , array2, 0, array.length);
+            array2[last + 1] = item;
+            array = new Object[last*2];
+            System.arraycopy(array2, 0 , array, 0, last);
             array2 = null;
+            return true;
         }
     }
 
-    public void add(int index, T item) {
+    public boolean add(int index, T item) {
         if(outOfBounds(index)) {
-            array2 = new Object[array.length + 1];
-            System.arraycopy(array, 0 , array2, 0, index - 1);
-            array2[index] = item;
-            System.arraycopy(array, index, array2, index + 1, array.length - index);
-            array = new Object[array.length + 1];
-            System.arraycopy(array2, 0, array, 0, array.length);
-            array2 = null;
+            if (last == array.length) {
+                array2 = new Object[last*2];
+                System.arraycopy(array, 0 , array2, 0, array.length);
+                array = new Object[last*2];
+                System.arraycopy(array2, 0 , array, 0, last);
+                array2 = null;
+            }
+            if (index == 0) {
+                array2 = new Object[array.length];
+                System.arraycopy(array, 1 , array2, 1, last - 1);
+                array2[0] = item;
+                System.arraycopy(array2, 0, array, 0, array.length);
+                array2 = null;
+                last++;
+                return true;
+            } else if (index == last) {
+                array2 = new Object[array.length];
+                System.arraycopy(array, 0 , array2, 0, last - 1);
+                array2[last] = item;
+                System.arraycopy(array2, 0, array, 0, array.length);
+                array2 = null;
+                last++;
+                return true;
+            } else {
+                array2 = new Object[array.length];
+                System.arraycopy(array, 0 , array2, 0, index - 1);
+                array2[index] = item;
+                System.arraycopy(array, index, array2, index + 1, array.length - index - 1);
+                System.arraycopy(array2, 0, array, 0, array.length);
+                array2 = null;
+                last++;
+                return true;
+            }
         } else {
             throw new IndexOutOfBoundsException("out of bounds");
         }
     }
 
-    public void set(int index, T item) {
+    public boolean set(int index, T item) {
         if(outOfBounds(index)) {
             array[index] = item;
+            return true;
         } else {
             throw new IndexOutOfBoundsException("out of bounds");
         }
     }
 
-    public void delete(int index) {
+    public boolean delete(int index) {
         if(outOfBounds(index)) {
             array2 = new Object[array.length];
-            System.arraycopy(array, 0, array2, 0 , index);
-            System.arraycopy(array, index + 1, array2, index, array.length - index);
+            System.arraycopy(array, 0, array2, 0, index);
+            System.arraycopy(array, index + 1, array2, index, array.length - 1);
             System.arraycopy(array2, 0, array, 0, array.length);
+            last--;
             array2 = null;
+            return true;
         } else {
             throw new IndexOutOfBoundsException("out of bounds");
         }
@@ -63,14 +100,18 @@ public class MyArrayList<T> {
     }
 
     public int length() {
-        return array.length;
+        return last;
+    }
+
+    public boolean sort() {
+        return true;
     }
 
     private boolean outOfBounds(int index) {
         if (index < 0) {
             return false;
         }
-        if (index < array.length) {
+        if (index <= last) {
             return true;
         } else {
             return false;
--- src/test/java/ru/pavel/algorithms/AppTest.java
+++ src/test/java/ru/pavel/algorithms/AppTest.java
@@ -2,23 +2,91 @@ package ru.pavel.algorithms;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
 
-    MyArrayList<Integer> arrayList = new MyArrayList<>();
+public class AppTest {
+    MyArrayList myArrayList;
+    @Before
+    public void setMyArrayList() {
+        myArrayList = new MyArrayList();
+    }
 
     @Test
-    public void shouldAnswerWithTrue()
+    public void addMyArrayListAdd()
     {
-        assertTrue( true );
+        assertTrue(myArrayList.add(1));
+    }
+
+    @Test
+    public void delMyArrayListAdd()
+    {
+        assertTrue(myArrayList.delete(0));
+    }
+
+    @Test
+    public void massAddMyArrayListAdd()
+    {
+        assertTrue(myArrayList.add(1));
+        assertTrue(myArrayList.add(2));
+        assertTrue(myArrayList.add(3));
+        assertTrue(myArrayList.add(4));
+        assertTrue(myArrayList.add(5));
+        assertTrue(myArrayList.add(6));
+        assertTrue(myArrayList.add(7));
+        assertTrue(myArrayList.add(8));
+        assertTrue(myArrayList.add(9));
+        assertTrue(myArrayList.add(10));
+        assertTrue(myArrayList.add(11));
+        assertTrue(myArrayList.add(12));
+        Assert.assertEquals(12, myArrayList.length() );
+    }
+
+    @Test
+    public void massAddDelMyArrayListAdd()
+    {
+        assertTrue(myArrayList.add(1));
+        assertTrue(myArrayList.add(2));
+        assertTrue(myArrayList.add(3));
+        assertTrue(myArrayList.add(4));
+        assertTrue(myArrayList.add(5));
+        assertTrue(myArrayList.delete(0));
+        Assert.assertEquals(4, myArrayList.length() );
+    }
+
+    @Test
+    public void getMyArrayListAdd()
+    {
+        assertTrue(myArrayList.add(10));
+        Assert.assertEquals(10, myArrayList.get(0) );
+    }
+
+    @Test
+    public void setMyArrayListAdd()
+    {
+        assertTrue(myArrayList.add(10));
+        assertTrue(myArrayList.set(0, 5));
+        Assert.assertEquals(5, myArrayList.get(0));
+    }
+
+    @Test
+    public void massAddIndexMyArrayListAdd()
+    {
+        assertTrue(myArrayList.add(1));
+        assertTrue(myArrayList.add(2));
+        assertTrue(myArrayList.add(3));
+        assertTrue(myArrayList.add(4));
+        assertTrue(myArrayList.add(0, 12));
+        assertTrue(myArrayList.add(5, 21));
+        Assert.assertEquals(12, myArrayList.get(0));
+        Assert.assertEquals(2, myArrayList.get(1));
+        Assert.assertEquals(21, myArrayList.get(5));
+        assertTrue(myArrayList.add(3, 9));
+        Assert.assertEquals(9, myArrayList.get(3));
     }
 }
