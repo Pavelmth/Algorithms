e2c9a2dc7cca8ee385fd484f7366f69db28d3de9
Pavel <pavelmth@gmail.com>
2019-03-09 23:54:12 +0300

    L04_fin

--- src/main/java/ru/pavel/algorithms/MyLinkedList.java
+++ src/main/java/ru/pavel/algorithms/MyLinkedList.java
@@ -1,4 +1,231 @@
 package ru.pavel.algorithms;
 
-public class MyLinkedList {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MyLinkedList<T> implements Iterable<T> {
+    private class Node {
+        T item;
+        Node next;
+        Node previous;
+
+        public Node(Node previous, T item, Node next) {
+            this.previous = previous;
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    private Node first = null;
+    private Node last = null;
+    private int size = 0;
+
+    @Override
+    public Iterator<T> iterator() {
+        return new MyLinkedListIterator();
+    }
+
+    private class MyLinkedListIterator implements Iterator<T> {
+
+        Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        @Override
+        public void remove() {
+
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public T getFirst() {
+        if (isEmpty()) throw new NoSuchElementException("List is empty");
+        return first.item;
+    }
+
+    public void addFirst(T item) {
+        Node oldFirst = first;
+        first = new Node(null, item, oldFirst);
+        if (isEmpty()) {
+            last = first;
+        } else {
+            oldFirst.previous = first;
+        }
+        size++;
+    }
+
+    public T removeFirst() {
+        if(isEmpty()) throw new NoSuchElementException("List is empty");
+        Node second = first.next;
+        T item = first.item;
+        first = second;
+        size--;
+        if (isEmpty()) {
+            last = null;
+        } else {
+            second.previous = null;
+        }
+        return item;
+    }
+
+    public T getLast() {
+        if (isEmpty()) throw new NoSuchElementException("List is empty");
+        return last.item;
+    }
+
+    public void addLast(T item) {
+        Node oldLast = last;
+        last = new Node(oldLast, item, null);
+        if (isEmpty()) {
+            first = last;
+        } else {
+            oldLast.next = last;
+        }
+        size++;
+    }
+
+    public T removeLast() {
+        if(isEmpty()) throw new NoSuchElementException("List is empty");
+        T item = last.item;
+        Node previois = last.previous;
+        last = previois;
+        size--;
+        if (isEmpty()) {
+            first = null;
+        } else {
+            last.next = null;
+        }
+        return item;
+    }
+
+    public T get(int index) {
+        if(index < 0 || index > size - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index < (size / 2)) {
+            int currentIndex = 0;
+            Node current = first;
+            while (currentIndex < index) {
+                current = current.next;
+                currentIndex++;
+            }
+            return current.item;
+        } else {
+            int currentIndex = size - 1;
+            Node current = last;
+            while (currentIndex > index) {
+                current = current.previous;
+                currentIndex--;
+            }
+            return current.item;
+        }
+    }
+
+    public void set(int index, T item) {
+        if(index < 0 || index > size - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        int currentIndex = 0;
+        Node current = first;
+        while (currentIndex < index) {
+            current = current.next;
+            currentIndex++;
+        }
+        current.item = item;
+    }
+
+    public int indexOf(T item) {
+        Node current = first;
+        int currentIndex = 0;
+        while (current != null && !current.item.equals(item)) {
+            current = current.next;
+            currentIndex++;
+        }
+        return current != null ? currentIndex : -1;
+    }
+
+    public boolean contains(T item) {
+        return indexOf(item) > -1;
+    }
+
+    public boolean remove(T item) {
+        Node current = first;
+        while (current != null && !current.item.equals(item)) {
+            current = current.next;
+        }
+        if (current == null) {
+            return false;
+        }
+        if (current == first) {
+            return true;
+        }
+        if (current == last) {
+            return true;
+        }
+        Node next = current.next;
+        Node previous = current.previous;
+        previous.next = next;
+        next.previous = previous;
+        size--;
+        current.previous = null;
+        current.next = null;
+        current.item = null;
+        return true;
+    }
+
+    public void add(int index, T item) {
+        if(index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            addFirst(item);
+            return;
+        } else if (index == size) {
+            addLast(item);
+            return;
+        }
+
+        int currentIndex = 0;
+        Node current = first;
+        while (currentIndex < index) {
+            current = current.next;
+            currentIndex++;
+        }
+        Node newNode = new Node(current.previous, item, current);
+        current.previous.next = newNode;
+        current.previous = newNode;
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        Node current = first;
+        while (current != null) {
+            stringBuilder.append(current.item.toString());
+            if(current.next != null) stringBuilder.append(", ");
+            current = current.next;
+        }
+        return stringBuilder.toString();
+    }
 }
--- src/test/java/ru/pavel/algorithms/MyLinkedListTest.java
+++ src/test/java/ru/pavel/algorithms/MyLinkedListTest.java
@@ -1,4 +1,196 @@
 package ru.pavel.algorithms;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.NoSuchElementException;
+
+@RunWith(Parameterized.class)
 public class MyLinkedListTest {
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+            {0, 0, 'a'},
+            {1, 1, 'b'},
+            {2, 2, 'c'},
+            {3, 3, 'd'},
+            {4, 4, 'e'},
+            {5, 5, 'f'},
+            {6, 6, 'g'}
+        });
+    }
+
+    private int a;
+    private int b;
+    private char c;
+    public MyLinkedListTest (int a, int b, char c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    MyLinkedList myLinkedList;
+
+    @Before
+    public void init() {
+        myLinkedList = new MyLinkedList();
+    }
+
+    @Test
+    public void testSize() {
+        Assert.assertEquals(0, myLinkedList.size());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        Assert.assertEquals(true, myLinkedList.isEmpty());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testException() {
+        myLinkedList.getFirst();
+    }
+
+    @Test
+    public void testAddFirstGetFirst() {
+        myLinkedList.addFirst(a);
+        Assert.assertEquals(b, myLinkedList.getFirst());
+    }
+
+    @Test
+    public void testAddFirstRemoveFirst() {
+        myLinkedList.addFirst(a);
+        Assert.assertEquals(b, myLinkedList.removeFirst());
+    }
+
+    @Test
+    public void testAddLastGetLast() {
+        myLinkedList.addLast(a);
+        Assert.assertEquals(b, myLinkedList.getLast());
+    }
+
+    @Test
+    public void testAddFirstGetFirstLastRemoveFirstLast() {
+        myLinkedList.addFirst('a');
+        myLinkedList.addFirst('b');
+        myLinkedList.addFirst('c');
+        myLinkedList.addFirst('d');
+        myLinkedList.addFirst('e');
+        Assert.assertEquals('e', myLinkedList.removeFirst());
+        Assert.assertEquals('a', myLinkedList.removeLast());
+        Assert.assertEquals('d', myLinkedList.getFirst());
+        Assert.assertEquals('b', myLinkedList.getLast());
+    }
+
+    @Test
+    public void testAddLastGetFirstLastRemoveFirstLast() {
+        myLinkedList.addLast('a');
+        myLinkedList.addLast('b');
+        myLinkedList.addLast('c');
+        myLinkedList.addLast('d');
+        myLinkedList.addLast('e');
+        Assert.assertEquals('a', myLinkedList.removeFirst());
+        Assert.assertEquals('e', myLinkedList.removeLast());
+        Assert.assertEquals('b', myLinkedList.getFirst());
+        Assert.assertEquals('d', myLinkedList.getLast());
+    }
+
+    @Test
+    public void testAddLstGet() {
+        myLinkedList.addLast(c);
+        myLinkedList.addLast(c);
+        myLinkedList.addLast(c);
+        myLinkedList.addLast(c);
+        myLinkedList.addLast(c);
+        myLinkedList.addLast(c);
+        myLinkedList.addLast(c);
+        Assert.assertEquals(c, myLinkedList.get(a));
+        Assert.assertEquals(c, myLinkedList.get(a));
+        Assert.assertEquals(c, myLinkedList.get(a));
+        Assert.assertEquals(c, myLinkedList.get(a));
+        Assert.assertEquals(c, myLinkedList.get(a));
+        Assert.assertEquals(c, myLinkedList.get(a));
+        Assert.assertEquals(c, myLinkedList.get(a));
+    }
+
+    @Test
+    public void testAddFirstSetGet() {
+        myLinkedList.addFirst('w');
+        myLinkedList.addFirst('w');
+        myLinkedList.addFirst('w');
+        myLinkedList.addFirst('w');
+        myLinkedList.addFirst('w');
+        myLinkedList.addFirst('w');
+        myLinkedList.addFirst('w');
+        myLinkedList.set(a, c);
+        Assert.assertEquals(c, myLinkedList.get(a));
+    }
+
+    @Test
+    public void testIndexOf() {
+        myLinkedList.addFirst('w');
+        myLinkedList.addFirst('w');
+        myLinkedList.addFirst('w');
+        myLinkedList.addFirst('w');
+        myLinkedList.addFirst('w');
+        myLinkedList.addFirst('w');
+        myLinkedList.addFirst('w');
+        myLinkedList.set(a, c);
+        Assert.assertEquals(a, myLinkedList.indexOf(c));
+    }
+
+    @Test
+    public void testContains() {
+        myLinkedList.addFirst('a');
+        myLinkedList.addFirst('b');
+        myLinkedList.addFirst('c');
+        Assert.assertTrue(myLinkedList.contains('a'));
+        Assert.assertTrue(myLinkedList.contains('b'));
+        Assert.assertTrue(myLinkedList.contains('c'));
+        Assert.assertFalse(myLinkedList.contains('d'));
+        Assert.assertFalse(myLinkedList.contains('e'));
+    }
+
+    @Test
+    public void testRemove() {
+        myLinkedList.addFirst('a');
+        myLinkedList.addFirst('b');
+        myLinkedList.addFirst('c');
+        Assert.assertTrue(myLinkedList.remove('b'));
+        Assert.assertFalse(myLinkedList.remove('b'));
+        Assert.assertEquals(2, myLinkedList.size());
+    }
+
+    @Test
+    public void testAdd() {
+        myLinkedList.addFirst('a');
+        myLinkedList.addFirst('b');
+        myLinkedList.add(1, 'c');
+        Assert.assertEquals('c', myLinkedList.get(1));
+    }
+
+    @Test
+    public void testToString() {
+        myLinkedList.addFirst("I");
+        myLinkedList.addLast("like");
+        myLinkedList.addLast("coke");
+        Assert.assertEquals("I, like, coke", myLinkedList.toString());
+    }
+
+    @Test
+    public void testIterator() {
+        myLinkedList.addLast('a');
+        myLinkedList.addLast('b');
+        myLinkedList.addLast('c');
+        myLinkedList.addLast('d');
+        myLinkedList.addLast('e');
+
+        for (Object o: myLinkedList) {
+            System.out.print(o + ", ");
+        }
+    }
 }
